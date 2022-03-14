0857dbcf4c5b453a75226a44d74b7155f5b15e80
Excel Olatunbosun <xclolatunbosun@gmail.com>
2022-03-15 00:07:22 +0100

    ''

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native'
+
+
+const SearchScreen = () => {
+    return (
+    <view>
+<Text>Search Screen</Text>
+    </view>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default SearchScreen;
