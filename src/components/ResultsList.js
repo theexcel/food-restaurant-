f47b70f9ae49cda3c7d5c7daa7c2e13ffa8a7d4f
Excel Olatunbosun <xclolatunbosun@gmail.com>
2022-03-16 18:26:46 +0100

    fixed a bug on this dman app I am building and it feels damn good

---
+++ src/components/ResultsList.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import ResultsDetail from './ResultsDetail';
+
+
+
+const ResultsList = ({ title, results }) => {
+  return (
+    <View>
+      <Text style = {styles.titleStyle}>{title}</Text>
+      <FlatList 
+       horizontal
+       data = {results}
+       keyExtractor = {(result) => result.id}
+       renderItem = {({item}) =>{
+            return <ResultsDetail result = {item}/>;
+       }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    titleStyle: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    }
+});
+
+export default ResultsList;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import useResults  from '../hooks/useResults'
+import ResultsList from '../components/ResultsList'
+
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [searchApi, results, errorMessage] = useResults();
 
+const filterResultsByPrice = (price) => {
+  return results.filter(result => {
+    return result.price === price;
+  })
+}
   return (
     <View>
       <SearchBar
@@ -16,6 +23,10 @@ const SearchScreen = () => {
        />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+
+      <ResultsList results = {filterResultsByPrice('$')} title = 'Cost Effective'/>
+      <ResultsList results = {filterResultsByPrice('$$')} title = 'Bit Pricier'/>
+      <ResultsList results = {filterResultsByPrice('$$$')} title = 'Big Spender'/>
     </View>
   );
 };
