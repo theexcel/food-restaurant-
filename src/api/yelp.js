3b60a643be0c6213b2ff7d24d8a0f89bcbf4383c
Excel Olatunbosun <xclolatunbosun@gmail.com>
2022-03-16 12:25:29 +0100

    fetching data

---
+++ src/api/yelp.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+
+export default axios.create({
+    baseURL: 'https://api.yelp.com/v3/businesses',
+    headers: {
+        Authorization: 
+        'Bearer ARaJOVBPGqazDK-vdVWLQ7tw6Uo3pHieCuG3f29Npmcx5FQEtczfElqGEjApyfHseqC1MWHglGq1OaZ7aZSWoA8XRv-bG3wNtDn0ZMfMZQYx08cDsHKrqZwZuEcvYnYx'
+    }
+});
