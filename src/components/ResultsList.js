b24f18acf1a7b078483ea819ec5129370ed14d38
Excel Olatunbosun <xclolatunbosun@gmail.com>
2022-03-18 12:02:52 +0100

    set up for the resultsshow screen

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,35 +1,47 @@
 import React from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
-import ResultsDetail from './ResultsDetail';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { withNavigation } from "react-navigation";
+import ResultsDetail from "./ResultsDetail";
 
-
-
-const ResultsList = ({ title, results }) => {
+const ResultsList = ({ title, results, navigation }) => {
   return (
-    <View style = {styles.container}>
-      <Text style = {styles.titleStyle}>{title}</Text>
-      <FlatList 
-       horizontal
-       data = {results}
-       keyExtractor = {(result) => result.id}
-       renderItem = {({item}) =>{
-            return <ResultsDetail result = {item}/>;
-       }}
+    <View style={styles.container}>
+      <Text style={styles.titleStyle}>{title}</Text>
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={results}
+        keyExtractor={(result) => result.id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("ResultsShow", {id: item.id})}
+            >
+              <ResultsDetail result={item} />
+            </TouchableOpacity>
+          );
+        }}
       />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    titleStyle: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginLeft: 15,
-        marginBottom: 5
-    },
-    container: {
-      marginBottom: 10
-    }
+  titleStyle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginLeft: 15,
+    marginBottom: 5,
+  },
+  container: {
+    marginBottom: 10,
+  },
 });
 
-export default ResultsList;
+export default withNavigation(ResultsList);
---
+++ src/screens/ResultsShowScreen.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import yelp from "../api/yelp";
+
+const ResultsShowScreen = ({ navigation }) => {
+    const [result, setResult] = useState(null)
+  const id = navigation.getParam("id");
+
+  const getResult = async (id) => {
+   const response =  await yelp.get(`/${id}`);
+   setResult(response.data);
+  };
+
+  useEffect(() => {
+    getResult(id)
+  }, [])
+  return (
+    <View>
+      <Text>Results Show</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ResultsShowScreen;
